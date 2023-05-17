28e522bfae8abe9ab8bc9993b560d3ada8d7f4e5
MaiiaHa <maiia.robeiko@ukr.net>
2023-05-17 22:34:16 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,60 +1,116 @@
-import React, { Component } from 'react';
+//================= hooks =================
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 import { FaSistrix } from 'react-icons/fa';
-// import { toast } from 'react-toastify';
 import Notiflix from 'notiflix'; // all modules
+// import { toast } from 'react-toastify';
 
-export default class Searchbar extends Component {
-  state = {
-    value: '',
+export default function Searchbar({ onSubmit }) {
+  const [value, setValue] = useState('');
+
+  const formChange = event => {
+    setValue(event.currentTarget.value.toLowerCase());
   };
 
-  formChange = event => {
-    this.setState({ value: event.currentTarget.value.toLowerCase() });
-    //.trim()
-  };
-
-  formSubmit = event => {
+  const formSubmit = event => {
     event.preventDefault();
-    if (this.state.value.trim() === '') {
+    if (value.trim() === '') {
       return Notiflix.Notify.info('Please enter your search');
       // return toast('Enter your search');
     }
-    this.props.onSubmit(this.state.value);
-    // в консоль при кліку на сабміт при тому, коли в App.User CL(data):
-    // console.log(this.state);
-    this.resetForm();
+    onSubmit(value);
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({ value: '' });
+  const resetForm = () => {
+    setValue('');
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.formSubmit}>
-          <button type="submit" className={css.SearchFormButton}>
-            <FaSistrix fill="blue" width={40} height={40} />
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.value}
-            onChange={this.formChange}
-            name="search"
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={formSubmit}>
+        <button type="submit" className={css.SearchFormButton}>
+          <FaSistrix fill="blue" width={40} height={40} />
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={value}
+          onChange={formChange}
+          name="search"
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+//=========================================
+// import React, { Component } from 'react';
+// import css from './Searchbar.module.css';
+// import PropTypes from 'prop-types';
+// import { FaSistrix } from 'react-icons/fa';
+// // import { toast } from 'react-toastify';
+// import Notiflix from 'notiflix'; // all modules
+
+// export default class Searchbar extends Component {
+//   state = {
+//     value: '',
+//   };
+
+//   formChange = event => {
+//     this.setState({ value: event.currentTarget.value.toLowerCase() });
+//     //.trim()
+//   };
+
+//   formSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.value.trim() === '') {
+//       return Notiflix.Notify.info('Please enter your search');
+//       // return toast('Enter your search');
+//     }
+//     this.props.onSubmit(this.state.value);
+//     // в консоль при кліку на сабміт при тому, коли в App.User CL(data):
+//     // console.log(this.state);
+//     this.resetForm();
+//   };
+
+//   resetForm = () => {
+//     this.setState({ value: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.Searchbar}>
+//         <form className={css.SearchForm} onSubmit={this.formSubmit}>
+//           <button type="submit" className={css.SearchFormButton}>
+//             <FaSistrix fill="blue" width={40} height={40} />
+//             <span className={css.SearchFormButtonLabel}>Search</span>
+//           </button>
+//           <input
+//             className={css.SearchFormInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.value}
+//             onChange={this.formChange}
+//             name="search"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
